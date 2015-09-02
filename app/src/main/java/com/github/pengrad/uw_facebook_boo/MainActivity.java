f19cdd2a4795c6cca2b93dced662e8d45525a857
Stas Parshin <s.a.parshin@gmail.com>
2015-09-02 15:16:08 +0300

    LoginButton callbacks

--- app/src/main/java/com/github/pengrad/uw_facebook_boo/MainActivity.java
+++ app/src/main/java/com/github/pengrad/uw_facebook_boo/MainActivity.java
@@ -1,19 +1,49 @@
 package com.github.pengrad.uw_facebook_boo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements FacebookCallback<LoginResult> {
 
+    private CallbackManager mCallbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
-
         LoginButton loginButton = (LoginButton) findViewById(R.id.loginButton);
+
+        mCallbackManager = CallbackManager.Factory.create();
+        loginButton.registerCallback(mCallbackManager, this);
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onSuccess(LoginResult loginResult) {
+        Toast.makeText(this, loginResult.getAccessToken().toString(), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onCancel() {
+        Toast.makeText(this, "Login canceled", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onError(FacebookException e) {
+        Toast.makeText(this, e.toString(), Toast.LENGTH_SHORT).show();
+    }
+
 }
