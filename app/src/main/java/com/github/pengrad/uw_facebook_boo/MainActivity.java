3e4b968b6ea25bdecd21e515f3bc884c93190959
Stas Parshin <s.a.parshin@gmail.com>
2015-09-02 15:09:28 +0300

    remove fragment, go to activity

--- app/src/main/java/com/github/pengrad/uw_facebook_boo/MainActivity.java
+++ app/src/main/java/com/github/pengrad/uw_facebook_boo/MainActivity.java
@@ -3,25 +3,17 @@ package com.github.pengrad.uw_facebook_boo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.facebook.login.widget.LoginButton;
+
 public class MainActivity extends AppCompatActivity {
 
-    private MainFragment mainFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        if (savedInstanceState == null) {
-            // Add the fragment on initial activity setup
-            mainFragment = new MainFragment();
-            getSupportFragmentManager()
-                    .beginTransaction()
-                    .add(android.R.id.content, mainFragment)
-                    .commit();
-        } else {
-            // Or set the fragment from restored state info
-            mainFragment = (MainFragment) getSupportFragmentManager()
-                    .findFragmentById(android.R.id.content);
-        }
+        setContentView(R.layout.activity_main);
+
+        LoginButton loginButton = (LoginButton) findViewById(R.id.loginButton);
     }
 }
