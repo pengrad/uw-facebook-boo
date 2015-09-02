206ef80acdb855a4418c6843cd94c376b8505532
Stas Parshin <s.a.parshin@gmail.com>
2015-09-02 16:52:16 +0300

    post item click listener

--- app/src/main/java/com/github/pengrad/uw_facebook_boo/SecondActivity.java
+++ app/src/main/java/com/github/pengrad/uw_facebook_boo/SecondActivity.java
@@ -4,14 +4,15 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
+import android.widget.Toast;
 
 import com.facebook.AccessToken;
 import com.facebook.GraphRequest;
 import com.facebook.GraphResponse;
+import com.github.pengrad.uw_facebook_boo.recyclerview.ItemClickListener;
 import com.google.gson.Gson;
 
-public class SecondActivity extends AppCompatActivity implements GraphRequest.Callback {
+public class SecondActivity extends AppCompatActivity implements GraphRequest.Callback, ItemClickListener<FeedData.Post> {
 
     private static final String TAG = "SecondActivity";
     private FeedRecyclerAdapter mFeedAdapter;
@@ -20,15 +21,18 @@ public class SecondActivity extends AppCompatActivity implements GraphRequest.Ca
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        initView();
+        getPosts();
+    }
 
-        mFeedAdapter = new FeedRecyclerAdapter(null);
+    private void initView() {
+
+        mFeedAdapter = new FeedRecyclerAdapter(this);
 
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setHasFixedSize(true);
         recyclerView.setAdapter(mFeedAdapter);
-
-        getPosts();
     }
 
     private void getPosts() {
@@ -46,5 +50,10 @@ public class SecondActivity extends AppCompatActivity implements GraphRequest.Ca
         String json = graphResponse.getJSONObject().toString();
         FeedData feedData = new Gson().fromJson(json, FeedData.class);
         mFeedAdapter.addAll(feedData.data);
+    }
+
+    @Override
+    public void onItemClick(FeedData.Post item) {
+        Toast.makeText(this, "Open second activity", Toast.LENGTH_SHORT).show();
     }
 }
